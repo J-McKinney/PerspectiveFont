5eb80a3611da187b60627e83443bceb3312659ac
Jesse McKinney <jessemckinney513@gmail.com>
2020-07-06 19:01:02 -0400

    yay

---
+++ src/PerspectiveFont/PerspectiveFont.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./PerspectiveFont.css";
+
+function PerspectiveFont() {
+  return (
+    <div className="perspective-text">
+      <div className="perspective-line">
+        <p></p>
+        <p>Pure CSS</p>
+      </div>
+      <div className="perspective-line">
+        <p>Pure CSS</p>
+        <p>Perspective</p>
+      </div>
+      <div className="perspective-line">
+        <p>Perspective</p>
+        <p>Text Effect</p>
+      </div>
+      <div className="perspective-line">
+        <p>Text Effect</p>
+        <p>Tutorial</p>
+      </div>
+      <div className="perspective-line">
+        <p>Tutorial</p>
+        <p></p>
+      </div>
+    </div>
+  );
+}
+
+export default PerspectiveFont;
