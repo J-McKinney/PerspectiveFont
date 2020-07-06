4eecaf040608b6e23d008f42b23b907162621a9c
Jesse McKinney <jessemckinney513@gmail.com>
2020-07-06 19:12:00 -0400

    yay

--- src/PerspectiveFont/PerspectiveFont.jsx
+++ src/PerspectiveFont/PerspectiveFont.jsx
@@ -3,26 +3,28 @@ import "./PerspectiveFont.css";
 
 function PerspectiveFont() {
   return (
-    <div className="perspective-text">
-      <div className="perspective-line">
-        <p></p>
-        <p>Pure CSS</p>
-      </div>
-      <div className="perspective-line">
-        <p>Pure CSS</p>
-        <p>Perspective</p>
-      </div>
-      <div className="perspective-line">
-        <p>Perspective</p>
-        <p>Text Effect</p>
-      </div>
-      <div className="perspective-line">
-        <p>Text Effect</p>
-        <p>Tutorial</p>
-      </div>
-      <div className="perspective-line">
-        <p>Tutorial</p>
-        <p></p>
+    <div className="pWrapper">
+      <div className="perspective-text">
+        <div className="perspective-line">
+          <p></p>
+          <p>Pure CSS</p>
+        </div>
+        <div className="perspective-line">
+          <p>Pure CSS</p>
+          <p>Perspective</p>
+        </div>
+        <div className="perspective-line">
+          <p>Perspective</p>
+          <p>Text Effect</p>
+        </div>
+        <div className="perspective-line">
+          <p>Text Effect</p>
+          <p>Tutorial</p>
+        </div>
+        <div className="perspective-line">
+          <p>Tutorial</p>
+          <p></p>
+        </div>
       </div>
     </div>
   );
