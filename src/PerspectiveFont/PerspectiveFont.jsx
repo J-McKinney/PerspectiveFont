3ccca2cbbfa51ad7d7cd9fe0cfb676d97619817c
Jesse McKinney <jessemckinney513@gmail.com>
2020-07-06 23:23:53 -0400

    yay

--- src/PerspectiveFont/PerspectiveFont.jsx
+++ src/PerspectiveFont/PerspectiveFont.jsx
@@ -3,28 +3,66 @@ import "./PerspectiveFont.css";
 
 function PerspectiveFont() {
   return (
-    <div className="pWrapper">
-      <div className="perspective-text">
-        <div className="perspective-line">
-          <p></p>
-          <p>Pure CSS</p>
-        </div>
-        <div className="perspective-line">
-          <p>Pure CSS</p>
-          <p>Perspective</p>
-        </div>
-        <div className="perspective-line">
-          <p>Perspective</p>
-          <p>Text Effect</p>
-        </div>
-        <div className="perspective-line">
-          <p>Text Effect</p>
-          <p>Tutorial</p>
-        </div>
-        <div className="perspective-line">
-          <p>Tutorial</p>
-          <p></p>
-        </div>
+    <div className="perspective-text">
+      <div className="perspective-line">
+        <p></p>
+        <p>
+          Edit <code>src/App.js</code>
+        </p>
+      </div>
+      <div className="perspective-line">
+        <p>
+          Edit <code>src/App.js</code>
+        </p>
+        <p> and save to reload.</p>
+      </div>
+      <div className="perspective-line">
+        <p> and save to reload.</p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </p>
+      </div>
+      <div className="perspective-line">
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Tutorial
+          </a>
+        </p>
+      </div>
+      <div className="perspective-line">
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Tutorial
+          </a>
+        </p>
+        <p></p>
       </div>
     </div>
   );
