28bd3a3d92d2ccb84374342f1b1861611326d64a
Jesse <jessemckinney513@gmail.com>
2021-08-17 21:21:02 -0400

    Updates

---
+++ precache-manifest.6fba1f1875c65eb9a5b9ed12a1618308.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4a70b1837d73003982232f514e68af6",
+    "url": "/PerspectiveFont/index.html"
+  },
+  {
+    "revision": "27bb2c41c3c8293af356",
+    "url": "/PerspectiveFont/static/css/main.95aec8da.chunk.css"
+  },
+  {
+    "revision": "019d2e8c55730d5652a1",
+    "url": "/PerspectiveFont/static/js/2.349179e2.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/PerspectiveFont/static/js/2.349179e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27bb2c41c3c8293af356",
+    "url": "/PerspectiveFont/static/js/main.2226053b.chunk.js"
+  },
+  {
+    "revision": "9af09348a342b1fc67ba",
+    "url": "/PerspectiveFont/static/js/runtime-main.d92befa5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/PerspectiveFont/static/media/logo.5d5d9eef.svg"
+  }
+]);
